2e047bc0443722ea49065b8bc47ac4d758f138a0
AhmedElshenawy0 <shenawy100@gmail.com>
2025-01-07 20:07:34 +0200

    deploy

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import { authOptions } from "@/lib/nextAuth";
 import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
-export const config = {
-  runtime: "edge", // Ensure compatibility with Next.js API routes
-};
+// export const config = {
+//   runtime: "edge", // Ensure compatibility with Next.js API routes
+// };
 export { handler as GET, handler as POST };
