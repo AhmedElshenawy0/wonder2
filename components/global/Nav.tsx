64160a35005eb1af88397909e5f8b02536182771
AhmedElshenawy0 <shenawy100@gmail.com>
2025-01-08 05:05:16 +0200

    Edit dashboard like

--- components/global/Nav.tsx
+++ components/global/Nav.tsx
@@ -73,126 +73,126 @@ const Nav = () => {
     <div className="fixed top-0 z-[999999999999] w-full ">
       <SubButton />
       <nav className="flex flex-col bg-white overflow-hidden">
-        {/* <div className=""> */}
-        <div className="border-b border-black h-fit mx-3 lg:mx-10 flex justify-between items-center min-h-[53px]">
-          <div className="flex gap-4 lg:gap-5 items-center">
-            {session.data?.user?.email ? (
-              <button
-                className="py-1 px-2 border border-black text-black hover:bg-black rounded-md hover:text-white transition text-sm font-semibold"
-                onClick={() => signOut()}
-              >
-                Signout
+        <div className="">
+          <div className="border-b border-black h-fit mx-3 lg:mx-10 flex justify-between items-center min-h-[53px]">
+            <div className="flex gap-4 lg:gap-5 items-center">
+              {session.data?.user?.email ? (
+                <button
+                  className="py-1 px-2 border border-black text-black hover:bg-black rounded-md hover:text-white transition text-sm font-semibold"
+                  onClick={() => signOut()}
+                >
+                  Signout
+                </button>
+              ) : (
+                <button
+                  className="py-1 px-2 border border-black text-black hover:bg-black rounded-md hover:text-white transition text-sm font-semibold"
+                  onClick={() => router.push("/register")}
+                >
+                  Signup
+                </button>
+              )}
+              <div className="relative">
+                <FiShoppingCart
+                  aria-label="Open Shopping Cart"
+                  className=" cursor-pointer"
+                  onClick={handleCartClick}
+                  style={{ fontSize: "15px" }}
+                />
+                {isClient && session.data?.user?.email && (
+                  <span className="absolute -top-2 -right-3 flex justify-center items-center w-[14px] h-[14px] text-[9px] rounded-full bg-green-800 text-white">
+                    {cart?.length ? cart.length : 0}
+                  </span>
+                )}
+              </div>
+            </div>
+
+            {openNav ? (
+              <button onClick={() => setOpenNav(false)}>
+                <IoClose className="lg:hidden absolute top-[50%] translate-y-[-50%] left-0 text-[20px] cursor-pointer" />
               </button>
             ) : (
-              <button
-                className="py-1 px-2 border border-black text-black hover:bg-black rounded-md hover:text-white transition text-sm font-semibold"
-                onClick={() => router.push("/register")}
-              >
-                Signup
+              <button onClick={() => setOpenNav(true)}>
+                <FaBarsStaggered className="lg:hidden absolute top-[50%] translate-y-[-50%] left-0 text-[20px] cursor-pointer" />
               </button>
             )}
-            <div className="relative">
-              <FiShoppingCart
-                aria-label="Open Shopping Cart"
-                className=" cursor-pointer"
-                onClick={handleCartClick}
-                style={{ fontSize: "15px" }}
-              />
-              {isClient && session.data?.user?.email && (
-                <span className="absolute -top-2 -right-3 flex justify-center items-center w-[14px] h-[14px] text-[9px] rounded-full bg-green-800 text-white">
-                  {cart?.length ? cart.length : 0}
-                </span>
-              )}
-            </div>
-          </div>
-
-          {openNav ? (
-            <button onClick={() => setOpenNav(false)}>
-              <IoClose className="lg:hidden absolute top-[50%] translate-y-[-50%] left-0 text-[20px] cursor-pointer" />
-            </button>
-          ) : (
-            <button onClick={() => setOpenNav(true)}>
-              <FaBarsStaggered className="lg:hidden absolute top-[50%] translate-y-[-50%] left-0 text-[20px] cursor-pointer" />
-            </button>
-          )}
-          <div
-            // ref={ref}
-            style={{
-              clipPath: openNav
-                ? "polygon(100% 0, 0 0, 0 100%, 100% 100%)"
-                : "",
-            }}
-            className="links-holder transition-all duration-500 z-20"
-          >
-            <ul className="flex lg:items-center links">
-              <li
-                className={`hidden lg:flex justify-center items-center ${
-                  pathName == "/" ? "border-b border-black font-semibold" : ""
-                }`}
-              >
-                <Link href="/" className="text-[13px] ">
-                  الرئيسية
-                </Link>
-              </li>
-              <li
-                className={`hidden lg:flex justify-center items-center ${
-                  pathName == "/products"
-                    ? "border-b border-black font-semibold"
-                    : ""
-                }`}
-              >
-                <Link href="/products" className="text-[13px] ">
-                  أحدث المنتجات
-                </Link>
-              </li>
-              <li
-                className={`hidden lg:flex justify-center items-center ${
-                  pathName == "/care-article"
-                    ? "border-b border-black font-semibold"
-                    : ""
-                }`}
-              >
-                <Link href="/care-article" className="text-[13px] ">
-                  المدونة
-                </Link>
-              </li>
-              <li
-                className={`hidden lg:flex justify-center items-center ${
-                  pathName == "/about"
-                    ? "border-b border-black font-semibold"
-                    : ""
-                }`}
-              >
-                <Link href="/about" className="text-[13px] ">
-                  من نحن
-                </Link>
-              </li>
-
-              {isClient && session?.data?.user?.isAdmin === true && (
-                <li className={`hidden lg:flex  justify-center items-center`}>
-                  <Link
-                    href="/admin"
-                    className="font-extrabold text-[13px] bg-black text-neutral-200 border border-black py-1 px-2 "
-                  >
-                    Dashboard
+            <div
+              // ref={ref}
+              style={{
+                clipPath: openNav
+                  ? "polygon(100% 0, 0 0, 0 100%, 100% 100%)"
+                  : "",
+              }}
+              className="links-holder transition-all duration-500 z-20"
+            >
+              <ul className="flex lg:items-center links">
+                <li
+                  className={`hidden lg:flex justify-center items-center ${
+                    pathName == "/" ? "border-b border-black font-semibold" : ""
+                  }`}
+                >
+                  <Link href="/" className="text-[13px] ">
+                    الرئيسية
                   </Link>
                 </li>
-              )}
-            </ul>
-            {/* <div className="rounded-lg flex flex-col justify-start"> */}
-            <CategoryDropDown openNav={openNav} setOpenNav={setOpenNav} />
-            {/* </div> */}
+                <li
+                  className={`hidden lg:flex justify-center items-center ${
+                    pathName == "/products"
+                      ? "border-b border-black font-semibold"
+                      : ""
+                  }`}
+                >
+                  <Link href="/products" className="text-[13px] ">
+                    أحدث المنتجات
+                  </Link>
+                </li>
+                <li
+                  className={`hidden lg:flex justify-center items-center ${
+                    pathName == "/care-article"
+                      ? "border-b border-black font-semibold"
+                      : ""
+                  }`}
+                >
+                  <Link href="/care-article" className="text-[13px] ">
+                    المدونة
+                  </Link>
+                </li>
+                <li
+                  className={`hidden lg:flex justify-center items-center ${
+                    pathName == "/about"
+                      ? "border-b border-black font-semibold"
+                      : ""
+                  }`}
+                >
+                  <Link href="/about" className="text-[13px] ">
+                    من نحن
+                  </Link>
+                </li>
+
+                {isClient && session?.data?.user?.isAdmin === true && (
+                  <li className={`hidden lg:flex  justify-center items-center`}>
+                    <Link
+                      href="/admin"
+                      className="font-extrabold text-[13px] bg-black text-neutral-200 border border-black py-1 px-2 "
+                    >
+                      Dashboard
+                    </Link>
+                  </li>
+                )}
+              </ul>
+              {/* <div className="rounded-lg flex flex-col justify-start"> */}
+              <CategoryDropDown openNav={openNav} setOpenNav={setOpenNav} />
+              {/* </div> */}
+            </div>
+            <Link
+              href="/"
+              className={`relative text-[18px] logo font-semibold max-lg:absolute max-lg:right-[50%] max-lg:top-[50%] max-lg:translate-y-[-50%] max-lg:translate-x-[50%]`}
+            >
+              <span className="absolute h-[3px] w-[70%] top-[10px] bg-black -right-2 z-[999999999]"></span>
+              <span className="absolute h-[3px] w-[70%] bottom-[10px] bg-black -left-2 z-[999999999]"></span>
+              RAVELLE
+            </Link>
           </div>
-          <Link
-            href="/"
-            className={`relative text-[18px] logo font-semibold max-lg:absolute max-lg:right-[50%] max-lg:top-[50%] max-lg:translate-y-[-50%] max-lg:translate-x-[50%]`}
-          >
-            <span className="absolute h-[3px] w-[70%] top-[10px] bg-black -right-2 z-[999999999]"></span>
-            <span className="absolute h-[3px] w-[70%] bottom-[10px] bg-black -left-2 z-[999999999]"></span>
-            RAVELLE
-          </Link>
         </div>
-        {/* </div> */}
       </nav>
     </div>
   );
